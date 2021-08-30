1e9ea62d933cdd8b0bb7bf6bd840fd0d820fc5ca
Bruno Paula <devbrunopaula@gmail.com>
2021-08-30 17:50:28 -0400

    adds navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+
+			blue: {
+				light: '#63B3ED',
+				DEFAULT: '#1fb6ff',
+				navBlue: '#2D3748',
+				dark: '#009eeb',
+			},
+			pink: {
+				light: '#D53F8C',
+			},
+			white: ' #FFF',
+		},
+
 		extend: {},
 	},
 	variants: {
