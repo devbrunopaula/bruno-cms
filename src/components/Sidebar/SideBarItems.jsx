224dcef53e76abfb289252a970a988a5680074f5
Bruno Paula <devbrunopaula@gmail.com>
2021-09-16 12:02:46 -0400

    Adds a sidebar active status

--- src/components/Sidebar/SideBarItems.jsx
+++ src/components/Sidebar/SideBarItems.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import bubble from '../../assets/images/Ellipse.png'
 
-function SideBarItems({Icon, title, show}) {
+import {NavLink, useLocation} from 'react-router-dom'
+
+function SideBarItems({data}) {
+	const [pathname, setPathname] = useState('/')
+	const location = useLocation()
+
+	useEffect(() => {
+		setPathname(location.pathname)
+	}, [location])
 	return (
 		<div className='flex mb-5 items-center'>
-			<div className='mr-3'>
-				<Icon className='h-5 w-5 text-blue-primary ' />
+			<div className='mr-3 flex items-center'>
+				<data.icon className='h-5 w-5 text-blue-primary mx-2' />
+				<NavLink
+					exact
+					className='w-36 text-lg'
+					to={data.path}
+					activeClassName='active-link-text'
+				>
+					{data.title}
+				</NavLink>
 			</div>
-			<div className='w-36 text-lg'>{title}</div>
+
 			<div className='flex items-center justify-end '>
-				{show ? <img src={bubble} alt={`${title}`} /> : ''}
+				{pathname === data.path ? (
+					<img src={bubble} alt={data.title} />
+				) : (
+					''
+				)}
 			</div>
 		</div>
 	)
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,59 +1,24 @@
-import React, {useState} from 'react'
-import {
-	HomeIcon,
-	DocumentAddIcon,
-	PencilAltIcon,
-	PhotographIcon,
-	UsersIcon,
-	LightningBoltIcon,
-	TrashIcon,
-	BookOpenIcon,
-	CubeIcon,
-	ChartBarIcon,
-} from '@heroicons/react/solid'
+import React from 'react'
 
 import './sidebar.scss'
 import SideBarItems from './SideBarItems'
-
-const ManageLinks = [
-	{icon: HomeIcon, title: 'View Site'},
-	{icon: DocumentAddIcon, title: 'Create page'},
-	{icon: PencilAltIcon, title: 'Blog articles'},
-	{icon: PhotographIcon, title: 'Files'},
-	{icon: UsersIcon, title: 'Users'},
-	{icon: LightningBoltIcon, title: 'Subscription'},
-	{icon: TrashIcon, title: 'Archived pages'},
-]
-
-const ProFeatures = [
-	{icon: BookOpenIcon, title: 'Themes'},
-	{icon: CubeIcon, title: 'Plugins'},
-	// {icon: ChartBarIcon, title: 'Plugins'},
-]
+import {ManageLinks, ProFeatures} from '../../route'
 
 function Sidebar() {
-	const [show, setShow] = useState(false)
 	return (
 		<div className='ml-10 mt-7'>
 			<h4 className='mb-9 text-blue-light'>Manage</h4>
 			<div className='flex flex-row items-center'>
 				<div className='flex flex-col w-full'>
 					{ManageLinks &&
-						ManageLinks.map(link => (
-							<SideBarItems
-								Icon={link.icon}
-								title={link.title}
-								status={show}
-							/>
+						ManageLinks.map((link, i) => (
+							<SideBarItems data={link} key={i} />
 						))}
+
 					<h4 className='mb-9 text-blue-light'>Pro features</h4>
 					{ProFeatures &&
-						ProFeatures.map(link => (
-							<SideBarItems
-								Icon={link.icon}
-								title={link.title}
-								status={show}
-							/>
+						ProFeatures.map((link, i) => (
+							<SideBarItems key={i} data={link} />
 						))}
 				</div>
 			</div>
